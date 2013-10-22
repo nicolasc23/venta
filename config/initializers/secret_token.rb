1459e5206abe5f921257ff9e15fa06a4907ead79
Nicolas <nicolasc23@gmail.com>
2013-10-22 19:42:34 -0300

    primer comentario

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Venta::Application.config.secret_token = '4e8e11c427ccb874f766fcec8d6fce0c92a7417860e1e3bff7cc07e8ae27d609eb65466f1dc2ccfcb16b31e8a1a6e709d025f2bf3f4cbc357b3706bca158a7ff'
